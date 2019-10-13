4d74ac0aa848664377a7739cdf26b652d03d80f5
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2019-10-13 10:35:00 +0200

    why is this cast necessary in printf?

--- minus1.cpp
+++ minus1.cpp
@@ -9,10 +9,10 @@ main()
     int16_t int16 = -1;
     uint32_t int32u = 65536;
     printf("method:%s:   signed -1 16 bits:%d, -1 32 bits=%d.\n", methodName.c_str(), int16, int32);
-    printf("method:%s: unsigned -1 16 bits:%u, -1 32 bits=%u.\n", methodName.c_str(), int16, int32);
+    printf("method:%s: unsigned -1 16 bits:%u, -1 32 bits=%u.\n", methodName.c_str(), static_cast<uint16_t>(int16), int32);
     const char* textYES = "YES";
     const char* textNO  = "NO";
-    printf("Method:%s: --\n"
+    printf("method:%s: --\n"
         " signed -1 16 bits equal to %d : %s.\n"
         " signed -1 16 bits equal to direct cast %u : %s.\n"
         , methodName.c_str()
